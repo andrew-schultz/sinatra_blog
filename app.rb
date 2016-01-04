b2279308d5d08b7491b5424d9eeedaea801c7c37
Andrew Schultz <as173171@gmail.com>
2016-01-04 18:51:18 -0500

    added profile and account erbs

--- app.rb
+++ app.rb
@@ -6,3 +6,15 @@ require "./models.rb"
 set :database, "sqlite3:blogdb.squlite3"
 
 enable :sessions
+
+
+get '/profile/<% user_id %>' do
+	
+	erb :profile
+end
+
+def current_user
+	if session[:user_id]
+		@current_user = User.find(session[:user_id])
+	end
+end
