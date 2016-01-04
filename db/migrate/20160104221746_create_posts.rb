b88a16341e202cbe6adc4f327d99f8f5d78cc837
Andrew Schultz <as173171@gmail.com>
2016-01-04 17:19:07 -0500

    added a posts table

---
+++ db/migrate/20160104221746_create_posts.rb
@@ -0,0 +1,10 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+  	create_table :posts do |t|
+  		t.string :title
+  		t.text :body
+  		t.timestamps null: false
+  		t.integer :user_id
+  	end
+  end
+end
