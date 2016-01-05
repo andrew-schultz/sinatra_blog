52841664285f4d1decb8cab2253d240c31dfea17
Andrew Schultz <as173171@gmail.com>
2016-01-05 14:45:42 -0500

    fleshed out the create a post method on the profile page, began to play around with styling. There's a basic sign in and layout page as well

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160104221746) do
+ActiveRecord::Schema.define(version: 20160105193252) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20160104221746) do
     t.text     "bio"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image"
   end
 
 end
